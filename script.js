69a71056622130ab13b1c09cac09007d42639e4d
Tar Zoltán <tarzoltan531@gmail.com>
2025-04-01 17:25:18 +0200

    init

---
+++ script.js
@@ -0,0 +1,65 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+const restartButton = document.getElementById("restartButton");
+
+const box = 20;
+let snake, direction, food, game;
+
+function initGame() {
+    snake = [{ x: 10 * box, y: 10 * box }];
+    direction = "RIGHT";
+    food = { 
+        x: Math.floor(Math.random() * 20) * box, 
+        y: Math.floor(Math.random() * 20) * box 
+    };
+    restartButton.style.display = "none";
+    game = setInterval(draw, 100);
+}
+
+document.addEventListener("keydown", changeDirection);
+restartButton.addEventListener("click", initGame);
+
+function changeDirection(event) {
+    if (event.key === "ArrowUp" && direction !== "DOWN") direction = "UP";
+    else if (event.key === "ArrowDown" && direction !== "UP") direction = "DOWN";
+    else if (event.key === "ArrowLeft" && direction !== "RIGHT") direction = "LEFT";
+    else if (event.key === "ArrowRight" && direction !== "LEFT") direction = "RIGHT";
+}
+
+function draw() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+    ctx.fillStyle = "red";
+    ctx.fillRect(food.x, food.y, box, box);
+    
+    ctx.fillStyle = "green";
+    snake.forEach(segment => ctx.fillRect(segment.x, segment.y, box, box));
+    
+    let newHead = { x: snake[0].x, y: snake[0].y };
+    if (direction === "UP") newHead.y -= box;
+    if (direction === "DOWN") newHead.y += box;
+    if (direction === "LEFT") newHead.x -= box;
+    if (direction === "RIGHT") newHead.x += box;
+    
+    if (newHead.x === food.x && newHead.y === food.y) {
+        food = { 
+            x: Math.floor(Math.random() * 20) * box, 
+            y: Math.floor(Math.random() * 20) * box 
+        };
+    } else {
+        snake.pop();
+    }
+    
+    if (newHead.x < 0 || newHead.x >= canvas.width || 
+        newHead.y < 0 || newHead.y >= canvas.height ||
+        snake.some(segment => segment.x === newHead.x && segment.y === newHead.y)) {
+        clearInterval(game);
+        restartButton.style.display = "block";
+        return;
+    }
+    
+    snake.unshift(newHead);
+}
+
+initGame();
